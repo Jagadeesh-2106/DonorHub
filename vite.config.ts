f57db693c198c371cc932bc8baca574f32e987de
Jagadeesh imandi <imandijagadeesh@gmail.com>
2026-07-10 00:19:36 +0530

    Update vite.config.ts to support dynamic base paths for dev and prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/DonorHub/',
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'serve' ? '/' : '/DonorHub/',
+    server: {
+      port: 3000,
+    },
+  };
 });
